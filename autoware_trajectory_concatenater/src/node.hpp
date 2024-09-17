0611ba28fcfbb612763e063c8b37e554583ae731
Satoshi OTA <44889564+satoshi-ota@users.noreply.github.com>
2024-09-17 18:59:19 +0900

    feat(concatenater): add new package (#11)
    Signed-off-by: satoshi-ota <satoshi.ota928@gmail.com>

---
+++ autoware_trajectory_concatenater/src/node.hpp
@@ -0,0 +1,63 @@
+// Copyright 2024 TIER IV, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef NODE_HPP_
+#define NODE_HPP_
+
+#include "trajectory_concatenater_param.hpp"
+
+#include <rclcpp/rclcpp.hpp>
+
+#include "autoware_new_planning_msgs/msg/trajectories.hpp"
+
+#include <map>
+#include <memory>
+#include <mutex>
+#include <string>
+#include <vector>
+
+namespace autoware::trajectory_selector::trajectory_concatenater
+{
+
+using namespace std::literals::chrono_literals;
+using autoware_new_planning_msgs::msg::Trajectories;
+using autoware_new_planning_msgs::msg::Trajectory;
+using autoware_new_planning_msgs::msg::TrajectoryGeneratorInfo;
+
+class TrajectoryConcatenaterNode : public rclcpp::Node
+{
+public:
+  explicit TrajectoryConcatenaterNode(const rclcpp::NodeOptions & node_options);
+
+private:
+  void process(const Trajectories::ConstSharedPtr msg, const std::string & topic_name);
+
+  void publish();
+
+  rclcpp::TimerBase::SharedPtr timer_;
+
+  rclcpp::Publisher<Trajectories>::SharedPtr pub_trajectores_;
+
+  std::shared_ptr<parameters::ParamListener> parameters_;
+
+  std::map<std::string, rclcpp::Subscription<Trajectories>::SharedPtr> subs_trajectories_;
+
+  std::map<std::string, Trajectories::ConstSharedPtr> buffer_;
+
+  mutable std::mutex mutex_;
+};
+
+}  // namespace autoware::trajectory_selector::trajectory_concatenater
+
+#endif  // NODE_HPP_
