0e49058513943534d35d180fb69d6cf4490174a0
Go Sakayori <go.sakayori@tier4.jp>
2025-02-14 15:11:15 +0900

    add time keeper for trajectory ranker
    Signed-off-by: Go Sakayori <go.sakayori@tier4.jp>

--- autoware_trajectory_ranker/src/node.cpp
+++ autoware_trajectory_ranker/src/node.cpp
@@ -40,7 +40,8 @@ TrajectoryRankerNode::TrajectoryRankerNode(const rclcpp::NodeOptions & node_opti
 : TrajectoryFilterInterface{"trajectory_ranker_node", node_options},
   pub_marker_{this->create_publisher<MarkerArray>("~/output/markers", 1)},
   pub_trajectories_debug_{this->create_publisher<TrajectoriesDebug>("~/output/score_debug", 1)},
-  pub_resampled_trajectories_{this->create_publisher<Trajectories>("~/output/resampled_trajectories", 1)},
+  pub_resampled_trajectories_{
+    this->create_publisher<Trajectories>("~/output/resampled_trajectories", 1)},
   listener_{std::make_unique<evaluation::ParamListener>(get_node_parameters_interface())},
   route_handler_{std::make_shared<RouteHandler>()},
   previous_points_{nullptr}
@@ -62,17 +63,28 @@ TrajectoryRankerNode::TrajectoryRankerNode(const rclcpp::NodeOptions & node_opti
   sub_route_ = create_subscription<LaneletRoute>(
     "~/input/route", rclcpp::QoS{1}.transient_local(),
     [this](const LaneletRoute::ConstSharedPtr msg) { route_handler_->setRoute(*msg); });
+
+  debug_processing_time_detail_pub_ =
+    create_publisher<autoware::universe_utils::ProcessingTimeDetail>(
+      "~/debug/processing_time_detail_ms/trajectory_ranker", 1);
+  time_keeper_ =
+    std::make_shared<autoware::universe_utils::TimeKeeper>(debug_processing_time_detail_pub_);
 }
 
 void TrajectoryRankerNode::process(const Trajectories::ConstSharedPtr msg)
 {
+  autoware::universe_utils::ScopedTimeTrack st(__func__, *time_keeper_);
+
   publish(score(msg));
-  publish_resampled_trajectory(msg); //// TODO(go-sakayori): remove this function when the ranker is stable
+  publish_resampled_trajectory(
+    msg);  //// TODO(go-sakayori): remove this function when the ranker is stable
 }
 
 auto TrajectoryRankerNode::score(const Trajectories::ConstSharedPtr msg)
   -> Trajectories::ConstSharedPtr
 {
+  autoware::universe_utils::ScopedTimeTrack st(__func__, *time_keeper_);
+
   if (!route_handler_->isHandlerReady()) {
     return msg;
   }
@@ -88,7 +100,7 @@ auto TrajectoryRankerNode::score(const Trajectories::ConstSharedPtr msg)
   }
 
   const auto steering_ptr = std::const_pointer_cast<SteeringReport>(sub_steering_.takeData());
-  if(steering_ptr == nullptr) {
+  if (steering_ptr == nullptr) {
     return msg;
   }
 
@@ -108,7 +120,8 @@ auto TrajectoryRankerNode::score(const Trajectories::ConstSharedPtr msg)
 
     const auto core_data = std::make_shared<CoreData>(
       std::make_shared<TrajectoryPoints>(t.points), std::make_shared<TrajectoryPoints>(points),
-      previous_points_, objects_ptr, odometry_ptr, steering_ptr, preferred_lanes, t.header, t.generator_id);
+      previous_points_, objects_ptr, odometry_ptr, steering_ptr, preferred_lanes, t.header,
+      t.generator_id);
 
     evaluator_->add(core_data);
   }
@@ -139,13 +152,21 @@ auto TrajectoryRankerNode::score(const Trajectories::ConstSharedPtr msg)
 
 void TrajectoryRankerNode::publish_resampled_trajectory(const Trajectories::ConstSharedPtr msg)
 {
+  autoware::universe_utils::ScopedTimeTrack st(__func__, *time_keeper_);
+
   std::vector<Trajectory> trajectories;
 
-  for(const auto & result : evaluator_->results()) {
-    const auto resampled_trajectory = autoware_new_planning_msgs::build<Trajectory>().header(result->header()).generator_id(result->uuid()).points(*result->points()).score(result->total());
-    trajectories.push_back(resampled_trajectory);  
+  for (const auto & result : evaluator_->results()) {
+    const auto resampled_trajectory = autoware_new_planning_msgs::build<Trajectory>()
+                                        .header(result->header())
+                                        .generator_id(result->uuid())
+                                        .points(*result->points())
+                                        .score(result->total());
+    trajectories.push_back(resampled_trajectory);
   }
-  const auto resampled_trajectories = autoware_new_planning_msgs::build<Trajectories>().trajectories(trajectories).generator_info(msg->generator_info);
+  const auto resampled_trajectories = autoware_new_planning_msgs::build<Trajectories>()
+                                        .trajectories(trajectories)
+                                        .generator_info(msg->generator_info);
   pub_resampled_trajectories_->publish(resampled_trajectories);
 }
 
