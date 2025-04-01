c6e3431a2c49b9a728e19e47873745d5e19c960f
Go Sakayori <go.sakayori@tier4.jp>
2025-04-01 18:44:34 +0900

    basic test implemented
    Signed-off-by: Go Sakayori <go.sakayori@tier4.jp>

--- autoware_feasible_trajectory_filter/test/test_util.cpp
+++ autoware_feasible_trajectory_filter/test/test_util.cpp
@@ -14,9 +14,37 @@
 
 #include "../src/utils.hpp"
 
+#include <autoware/trajectory_selector_common/type_alias.hpp>
+#include <autoware_utils_uuid/uuid_helper.hpp>
+#include <builtin_interfaces/msg/detail/time__struct.hpp>
+
 #include <gtest/gtest.h>
+
+#include <vector>
 
 namespace autoware::trajectory_selector::feasible_trajectory_filter
 {
+TEST(FeasibleTrajectoryFilterUtilsTest, is_trajectory_offtrack)
+{
+  const auto time = builtin_interfaces::build<builtin_interfaces::msg::Time>().sec(0).nanosec(0);
+  const auto header = std_msgs::build<std_msgs::msg::Header>().stamp(time).frame_id("map");
+  {
+    const auto ego_point = geometry_msgs::build<geometry_msgs::msg::Point>().x(0.0).y(0.0).z(0.0);
 
+    TrajectoryPoints points;
+    for (size_t i = 0; i < 10; i++) {
+      TrajectoryPoint point;
+      points.push_back(point);
+    }
+
+    const auto trajectory =
+      autoware_new_planning_msgs::build<autoware_new_planning_msgs::msg::Trajectory>()
+        .header(header)
+        .generator_id(autoware_utils_uuid::generate_uuid())
+        .points(points)
+        .score(0.0);
+
+    EXPECT_FALSE(utils::is_trajectory_offtrack(trajectory, ego_point));
+  }
+}
 }  // namespace autoware::trajectory_selector::feasible_trajectory_filter
