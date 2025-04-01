9885685835a82b0e6899d747ca502e3f26c8d993
Go Sakayori <go.sakayori@tier4.jp>
2025-04-01 16:12:29 +0900

    check path offtracking instead of only checking the origin
    Signed-off-by: Go Sakayori <go.sakayori@tier4.jp>

--- autoware_feasible_trajectory_filter/src/node.hpp
+++ autoware_feasible_trajectory_filter/src/node.hpp
@@ -38,7 +38,9 @@ private:
 
   Trajectories::ConstSharedPtr check_feasibility(const Trajectories::ConstSharedPtr msg);
 
-  bool check_trajectory_origin(const autoware_new_planning_msgs::msg::Trajectory & trajectory);
+  bool is_trajectory_offtrack(
+    const autoware_new_planning_msgs::msg::Trajectory & trajectory,
+    const geometry_msgs::msg::Point & ego_position);
 
   bool out_of_lane(const autoware_new_planning_msgs::msg::Trajectory & trajectory);
 
